89f725d2702be2f97ebbae447d254b0f25c7e1f8
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2024-11-20 17:35:54 +0530

    new: weighted-avg

---
+++ tests/unit/MathUtils.t.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '../BaseTest.t.sol';
+
+contract MathUtilsTest is BaseTest {
+  /// forge-config: default.fuzz.runs = 10000
+  function testFuzzNewWeightedAverageAdd(uint256[] memory numbers) public {
+    vm.assume(numbers.length > 0);
+
+    uint256 currentWeightedSum;
+    uint256 currentSumWeights;
+    uint256 currentWeightedAvg;
+
+    uint256 calcWeightedAvg;
+    uint256 calcSumWeights;
+    uint256 number;
+    uint256 weight;
+
+    for (uint256 i; i < numbers.length; ++i) {
+      // truncate
+      number = numbers[i] % type(uint128).max;
+      weight = numbers[i] % 100_00; // bps
+
+      calcWeightedAvg += number * weight;
+      calcSumWeights += weight;
+
+      (currentWeightedSum, currentSumWeights, currentWeightedAvg) = MathUtils.addToWeightedAverage(
+        currentWeightedSum,
+        currentSumWeights,
+        number,
+        weight
+      );
+    }
+    if (calcSumWeights != 0) {
+      calcWeightedAvg /= calcSumWeights;
+    }
+
+    assertEq(currentWeightedAvg, calcWeightedAvg);
+    assertEq(currentSumWeights, calcSumWeights);
+  }
+
+  /// forge-config: default.fuzz.runs = 10000
+  function testFuzzNewWeightedAverageRemove(uint256[] memory numbers, uint256 toRemoveIdx) public {
+    vm.assume(numbers.length > 1);
+    toRemoveIdx = bound(toRemoveIdx, 0, numbers.length - 1);
+
+    uint256 currentWeightedSum;
+    uint256 currentSumWeights;
+    uint256 currentWeightedAvg;
+
+    uint256 calcWeightedAvg;
+    uint256 calcSumWeights;
+    uint256 number;
+    uint256 weight;
+
+    for (uint256 i; i < numbers.length; ++i) {
+      // truncate
+      number = numbers[i] % type(uint128).max;
+      weight = numbers[i] % 100_00; // bps
+
+      if (i != toRemoveIdx) {
+        calcWeightedAvg += number * weight;
+        calcSumWeights += weight;
+      }
+
+      (currentWeightedSum, currentSumWeights, ) = MathUtils.addToWeightedAverage(
+        currentWeightedSum,
+        currentSumWeights,
+        number,
+        weight
+      );
+    }
+
+    if (calcSumWeights != 0) {
+      calcWeightedAvg /= calcSumWeights;
+    }
+
+    uint256 newValue = numbers[toRemoveIdx] % type(uint128).max;
+    uint256 newValueWeight = numbers[toRemoveIdx] % 100_00;
+
+    if (currentWeightedSum < newValue * newValueWeight) {
+      vm.expectRevert();
+      MathUtils.subtractFromWeightedAverage(
+        currentWeightedSum,
+        currentSumWeights,
+        newValue,
+        newValueWeight
+      );
+    } else {
+      (, currentSumWeights, currentWeightedAvg) = MathUtils.subtractFromWeightedAverage(
+        currentWeightedSum,
+        currentSumWeights,
+        newValue,
+        newValueWeight
+      );
+
+      assertEq(currentWeightedAvg, calcWeightedAvg);
+      assertEq(currentSumWeights, calcSumWeights);
+    }
+  }
+}
