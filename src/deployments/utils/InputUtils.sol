9f6c70ddb94bdb83ad9eb9a5de325d5cd02128ff
Kogaroshi <vigervalentin@gmail.com>
2025-11-05 16:01:03 +0100

    feat : Input helper for Deploy Engine

---
+++ src/deployments/utils/InputUtils.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/StdJson.sol';
+import 'forge-std/Vm.sol';
+
+contract InputUtils {
+  using stdJson for string;
+
+  struct FullDeployInputs {
+    address admin;
+    address nativeWrapperAddress;
+    string[] hubLabels;
+    string[] spokeLabels;
+  }
+
+  struct SpokeDeployInputs {
+    address admin;
+    string spokeLabel;
+  }
+
+  struct HubDeployInputs {
+    address admin;
+    string hubLabel;
+  }
+
+  Vm private constant vm = Vm(address(bytes20(uint160(uint256(keccak256('hevm cheat code'))))));
+
+  function loadFullDeployInputs(
+    string memory inputPath
+  ) public view returns (FullDeployInputs memory) {
+    string memory json = vm.readFile(inputPath);
+    bytes memory data = vm.parseJson(json);
+    FullDeployInputs memory inputs = abi.decode(data, (FullDeployInputs));
+    return inputs;
+  }
+
+  function loadSpokeDeployInputs(
+    string memory inputPath
+  ) public view returns (SpokeDeployInputs memory) {
+    string memory json = vm.readFile(inputPath);
+    bytes memory data = vm.parseJson(json);
+    SpokeDeployInputs memory inputs = abi.decode(data, (SpokeDeployInputs));
+    return inputs;
+  }
+
+  function loadHubDeployInputs(
+    string memory inputPath
+  ) public view returns (HubDeployInputs memory) {
+    string memory json = vm.readFile(inputPath);
+    bytes memory data = vm.parseJson(json);
+    HubDeployInputs memory inputs = abi.decode(data, (HubDeployInputs));
+    return inputs;
+  }
+}
