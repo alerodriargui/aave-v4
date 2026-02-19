eba0c6ff24db00095ab59561a2c9d8f2171cd639
YBM <31329384+yan-man@users.noreply.github.com>
2026-02-19 17:34:56 -0600

    fix: consolidate helpers; pr comments

--- src/deployments/utils/libraries/Create2Utils.sol
+++ src/deployments/utils/libraries/Create2Utils.sol
@@ -59,7 +59,7 @@ library Create2Utils {
     bytes32 salt,
     bytes memory bytecode
   ) internal pure returns (address) {
-    return computeCreate2Address(salt, keccak256(abi.encodePacked(bytecode)));
+    return computeCreate2Address(salt, keccak256(bytecode));
   }
 
   function addressFromLast20Bytes(bytes32 bytesValue) internal pure returns (address) {
