98e9fcb59881c473d79b1281224aa9daaf3c0f54
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-19 00:09:00 -0500

    chore: prettier

--- tests/utils/BatchTestProcedures.sol
+++ tests/utils/BatchTestProcedures.sol
@@ -10,9 +10,7 @@ import {Ownable} from 'src/dependencies/openzeppelin/Ownable.sol';
 import {IAccessManaged} from 'src/dependencies/openzeppelin/IAccessManaged.sol';
 
 // orchestration
-import {
-  AaveV4DeployOrchestration
-} from 'src/deployments/orchestration/AaveV4DeployOrchestration.sol';
+import {AaveV4DeployOrchestration} from 'src/deployments/orchestration/AaveV4DeployOrchestration.sol';
 import {WETHDeployProcedure} from 'tests/deployments/procedures/WETHDeployProcedure.sol';
 import {AaveV4TestOrchestration} from 'tests/deployments/orchestration/AaveV4TestOrchestration.sol';
 import {AaveV4DeployProcedureBase} from 'src/deployments/procedures/AaveV4DeployProcedureBase.sol';
