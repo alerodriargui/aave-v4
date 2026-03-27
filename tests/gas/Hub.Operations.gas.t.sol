7854d1126993e2f3d8df5c2eb676d7680c055b6d
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-27 14:46:17 -0500

    chore: cleanup, pr comments, rm vault ref

--- tests/gas/Hub.Operations.gas.t.sol
+++ tests/gas/Hub.Operations.gas.t.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'tests/gas/Base.gas.t.sol';
+import 'tests/setup/Base.t.sol';
 
 /// forge-config: default.isolate = true
-contract HubOperations_Gas_Tests is BaseGasTest {
+contract HubOperations_Gas_Tests is Base {
   using SafeCast for *;
   using WadRayMath for uint256;
 
