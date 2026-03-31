bdc46bafe3059879a7bcbaa0c17b5a7e439b3e63
YBM <31329384+yan-man@users.noreply.github.com>
2026-03-30 20:46:04 -0500

    chore: natspec; rm unneeded import

--- scripts/deploy/AaveV4DeployEthereum.s.sol
+++ scripts/deploy/AaveV4DeployEthereum.s.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {AaveV4DeployBatchBaseScript} from 'scripts/deploy/AaveV4DeployBatchBase.s.sol';
-import {Create2Utils} from 'src/deployments/utils/libraries/Create2Utils.sol';
 
-/// @notice Ethereum deploy script with hardcoded inputs for local testing.
+/// @notice Ethereum deploy script upon which custom deployment scripts can extend.
 abstract contract AaveV4DeployEthereum is AaveV4DeployBatchBaseScript {
   constructor() AaveV4DeployBatchBaseScript('ethereum') {}
 
